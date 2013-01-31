30b6e745ad81cb0dc484ff93d291bbcfa1333d90
Ben Doyle <ben.doyle@shopify.com>
2013-01-31 16:09:15 -0500

    attempting to establish an interface

--- test/multi_sort_test.rb
+++ test/multi_sort_test.rb
@@ -2,9 +2,24 @@ require 'test_helper'
 
 class MultiSortTest < Test::Unit::TestCase
   def setup
+    list_data = Array.new(50){TestDocument.new}
+    @source_a = TestSource.new(:a, list_data, lambda{|obj|obj.a})
+    @source_b = TestSource.new(:b, list_data, lambda{|obj|obj.b})
+    @source_c = TestSource.new(:c, list_data, lambda{|obj|obj.c})
   end
 
   def test_case
-    assert false
+    srt = Multisort.new(@source_a, @source_b)
+    srt.source(:b).weight = 2
+    srt.add(@source_c, 1)
+    result = srt.to_a[0...10]
+
+    assert_kind_of Enumerable, result 
+    assert_equal 10, result.size
+    assert_equal TestDocument, result.first[:doc].class
+    assert_equal TestSource,   result.first[:source].class
+    assert_equal Hash,   result.first[:other_sources].class
+    assert_equal TestSource,   result.first[:other_sources].first.first
+    assert_equal Fixnum,   result.first[:other_sources].first.last
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,24 @@
 require 'test/unit'
 require 'multi_sort'
+
+  class TestDocument
+    attr_reader :a, :b, :c
+    def intialize
+       @a = rand + rand
+       @b = 2 * rand # same mean as @a, but more varaiance
+       @c = 3 + rand - @a # anti-correlated with @a
+    end
+  end
+
+  class TestSource
+    attr_reader :name
+    def intialize(name, data, value_proc)
+      @name = name
+      @sort_block = sort_block
+      @data = data
+    end
+
+    def pull(limit)
+      @data.sort_by{|d|value_proc.call(d)}[0...limit]
+    end
+  end
